9c638be016f8dcbb0082a153ab7be04f40529130
Raffael Brunno <34611785+raffaelzzz@users.noreply.github.com>
2021-03-25 18:41:13 -0300

    Create Objeto_metodoPrivate.php

---
+++ Objeto_metodoPrivate.php
@@ -0,0 +1,62 @@
+<?php
+
+//Estrutura objeto
+
+class produto{
+
+    private $descricao;
+    private $estoque;
+    private $preco;
+
+
+//Devido a utilização do private, precisamos utilizar o setters(gravação) e getters(Leitura)
+//Métodos são publicos, ou seja, são acessíveis de fora da classe
+//Os atributos são privates
+
+
+//Método gravação
+
+    public function setdescricao($descricao){
+
+        if (is_string($descricao)) {
+            $this->descricao = $descricao;
+        }
+    }
+
+    public function setestoque($estoque){
+
+        if(is_numeric($estoque) AND $estoque >= 0){
+            
+            $this -> estoque = $estoque;
+        }
+    }
+
+    //Método leitura
+
+    public function getdescricao(){
+
+        return $this -> descricao;
+
+    }
+
+    public function getestoque(){
+
+        return $this -> estoque;
+
+    }
+}
+
+//Modo escrita setters(gravação) 
+$p1 = new produto;
+$p1 -> setdescricao('Chocolate');
+$p1 -> setestoque(18);
+
+//Modo leitura getters(Leitura)
+print 'Descrição: ' . $p1->getdescricao() . '<br>'.PHP_EOL;
+print 'Estoque: ' . $p1->getestoque() . '<br>'.PHP_EOL;
+
+
+
+
+
+?>
